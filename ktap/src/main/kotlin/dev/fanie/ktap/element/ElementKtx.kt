9a4b1ddde328d4e3564744679029ebaccdbad13b
Flavio Veizi <flaviov@spotify.com>
2020-06-01 09:30:45 +0300

    Fix optional when primitive

--- ktap/src/main/kotlin/dev/fanie/ktap/element/ElementKtx.kt
+++ ktap/src/main/kotlin/dev/fanie/ktap/element/ElementKtx.kt
@@ -5,14 +5,14 @@ import org.jetbrains.annotations.NotNull
 import javax.lang.model.element.ExecutableElement
 import javax.lang.model.element.VariableElement
 
-fun ExecutableElement.isOptional() = this.getAnnotation(NotNull::class.java) == null
+fun ExecutableElement.isOptional() = this.getAnnotation(NotNull::class.java) == null && !returnType.kind.isPrimitive
 
 val ExecutableElement.returnKType: KTypeMirror
     get() = object : KTypeMirror(returnType) {
         override val isOptional: Boolean get() = this@returnKType.isOptional()
     }
 
-fun VariableElement.isOptional() = this.getAnnotation(NotNull::class.java) == null
+fun VariableElement.isOptional() = this.getAnnotation(NotNull::class.java) == null && !asType().kind.isPrimitive
 
 fun VariableElement.asKType(): KTypeMirror = object : KTypeMirror(asType()) {
     override val isOptional: Boolean get() = this@asKType.isOptional()
